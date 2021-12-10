ec30cda5ea408ac5d0ab2292803a2155093bbf50
xR4V3 <shieldcoma@mail.ru>
2021-12-10 22:26:30 +0300

    ...

---
+++ koleanbox/shadowmoney/database/database.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connection(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func sqlUsers(DB db) {
+	insert, err :=
+}
